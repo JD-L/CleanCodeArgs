88c1e649e639a5301f7acbe6905838a6e754db39
wscheng <wscheng@cs.nctu.edu.tw>
2014-11-02 22:36:52 +0800

    Add Draft v1 and Only Boolean Version

---
+++ src/wscheng/cleancode/example/args/ArgsTest.java
@@ -0,0 +1,23 @@
+package wscheng.cleancode.example.args;
+
+import junit.framework.TestCase;
+
+public class ArgsTest extends TestCase {
+    public void testCreateWithNoSchemaOrArguments() throws Exception {
+        Args args = new Args("", new String[0]);
+        assertEquals(0, args.cardinality());
+    }
+
+    public void testOneBooleanArguments() throws Exception {
+        Args args = new Args("b", new String[] {"-b"});
+        assertEquals(true, args.getBoolean('b'));
+    }
+
+    public void testTwoBooleanArguments() throws Exception {
+        Args args = new Args("b,c", new String[] {"-b", "-c"});
+        assertEquals(true, args.getBoolean('b'));
+        assertEquals(true, args.getBoolean('c'));
+        // will get NPE
+        // assertEquals(false, args.getBoolean('d'));
+    }
+}
